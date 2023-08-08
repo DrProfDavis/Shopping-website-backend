f0f571fb151635548825fe5162f1826bb51c701e
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-08-08 14:02:51 -0500

    category-routes.js worked on

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,21 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
